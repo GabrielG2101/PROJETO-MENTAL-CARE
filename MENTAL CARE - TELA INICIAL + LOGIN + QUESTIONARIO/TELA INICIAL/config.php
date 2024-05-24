0658dcc239f9c11a14099964e5938ecd06ef85a8
GabrielG2101 <134340580+GabrielG2101@users.noreply.github.com>
2024-05-24 10:02:12 -0300

    Add files via upload

---
+++ MENTAL CARE - TELA INICIAL + LOGIN + QUESTIONARIO/TELA INICIAL/config.php
@@ -0,0 +1,6 @@
+<?php
+$db['host'] = "localhost"; // Host name
+$db['user'] = "root"; // Mysql username
+$db['pass'] = "YourPassword"; // Mysql password
+$db['name'] = "YourDBname"; // Database name
+?>
